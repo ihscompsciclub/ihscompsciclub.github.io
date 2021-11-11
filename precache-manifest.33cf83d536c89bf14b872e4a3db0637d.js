e8b2debfcc15b94a04693629ced2ea24b1cdd246
chrehall68 <hallchristopher8697@students.esuhsd.org>
2021-11-11 14:43:46 -0800

    Updates

---
+++ precache-manifest.33cf83d536c89bf14b872e4a3db0637d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec8126f74bf4e6c195f8234ada3ede2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2513e57358205662db5a",
+    "url": "/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "8189efa9864ff78956f9",
+    "url": "/static/css/main.8679f56e.chunk.css"
+  },
+  {
+    "revision": "2513e57358205662db5a",
+    "url": "/static/js/2.aae13457.chunk.js"
+  },
+  {
+    "revision": "119ff7b0e6dbb852f63360f69938dd73",
+    "url": "/static/js/2.aae13457.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bad0b55902c84d55e1b",
+    "url": "/static/js/3.78443ac8.chunk.js"
+  },
+  {
+    "revision": "8189efa9864ff78956f9",
+    "url": "/static/js/main.afd83e69.chunk.js"
+  },
+  {
+    "revision": "a34b8e0f5d4eba3c6778",
+    "url": "/static/js/runtime-main.bd5ef9e3.js"
+  }
+]);
