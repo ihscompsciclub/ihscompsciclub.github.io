b72ab2ddf3e6bcde90d86c9f5177c3ccaeade9e5
Andy <andyphung64@gmail.com>
2022-03-26 17:14:33 +0000

    Updates

---
+++ precache-manifest.6fa66e4cea5ab90c77cfde4cce46f928.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40c21da3e76d4eb59e30a9ae2ac796b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8a95840fb8e576f1450",
+    "url": "/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "58a61cf1825d9ae2bd63",
+    "url": "/static/css/main.640b9f69.chunk.css"
+  },
+  {
+    "revision": "a8a95840fb8e576f1450",
+    "url": "/static/js/2.f5696073.chunk.js"
+  },
+  {
+    "revision": "119ff7b0e6dbb852f63360f69938dd73",
+    "url": "/static/js/2.f5696073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09086c3a9a25e6ba0ba9",
+    "url": "/static/js/3.4f26dc4e.chunk.js"
+  },
+  {
+    "revision": "58a61cf1825d9ae2bd63",
+    "url": "/static/js/main.c158dbae.chunk.js"
+  },
+  {
+    "revision": "657d4bd83dc57b263e04",
+    "url": "/static/js/runtime-main.07fd16f5.js"
+  },
+  {
+    "revision": "476f4b478c36be0f7563ecbca53023d7",
+    "url": "/static/media/stickermule_logo.476f4b47.png"
+  }
+]);
